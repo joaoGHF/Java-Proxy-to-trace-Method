03b719150604155b2659cfd6aed97b95c6d62723
João Gabriel Horsth Fonseca <99850827+joaoGHF@users.noreply.github.com>
2023-08-16 00:58:50 +0000

    Initializing project

---
+++ ProxyTest/src/org.proxy/ProxyTest.java
@@ -0,0 +1,11 @@
+package org.proxy;
+
+import java.lang.reflect.*;
+import java.util.*;
+
+/**
+ * This program demonstrates the use of proxies.
+ * By the listing 6.10 of Core Java I
+ * @version 1.00 2023-08-15
+ * @author joaoGHF
+ */
